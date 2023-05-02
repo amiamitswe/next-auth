5cf597486187017af0495daccc717f72e06af8b4
Amit Samadder <amiamitswe@gmail.com>
2023-05-02 16:02:10 +0600

    add github auth using next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <div className="mx-auto max-w-7xl p-6 lg:px-8">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
