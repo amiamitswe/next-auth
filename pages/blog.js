3a1d6df8df25c533fc0631fce1725d4245bcce24
Amit Samadder <amiamitswe@gmail.com>
2023-05-02 18:52:51 +0600

    server side auth

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,7 @@
 import { getSession } from "next-auth/react";
 import React from "react";
 
-function Blog({data}) {
+function Blog({ data }) {
   return <div>Blog of {data}</div>;
 }
 
@@ -9,6 +9,16 @@ export default Blog;
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: { data: session ? "100 list data" : "free list data" },
   };
