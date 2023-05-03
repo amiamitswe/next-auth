7ac2ddf492c7d173934aff3fe8808faa50741a7e
Amit Samadder <amiamitswe@gmail.com>
2023-05-03 13:01:47 +0600

    mongodb add but not work

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,20 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  database: process.env.MDB_URL,
+  session: {
+    jwt: true,
+  },
+
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET
+  },
 });
